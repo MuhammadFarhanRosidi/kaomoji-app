2935949538ea20d962ef26d45e6cc2237807ec9f
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-10-21 11:17:03 +0700

    feat: add root sibling parent to active the toast in all screens

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Header from "./components/Header";
 import { globalStyles } from "./styles/globalStyles";
 import KaomojiLists from "./components/KaomojiLists";
+import { RootSiblingParent } from "react-native-root-siblings";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={globalStyles.container}>
-        <StatusBar style="dark" backgroundColor="#232530" />
-        <Header />
-        <KaomojiLists />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <RootSiblingParent>
+      <SafeAreaProvider>
+        <SafeAreaView style={globalStyles.container}>
+          <StatusBar style="dark" backgroundColor="#232530" />
+          <Header />
+          <KaomojiLists />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </RootSiblingParent>
   );
 }
